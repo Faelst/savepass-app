3d62d96726165812f49dcea70ab963e0c94e4d3a
Rafael Silverio <fael_st@hotmail.com>
2023-06-21 13:29:40 -0300

    finish challenge

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,115 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
+
+import { Header } from '../../components/Header';
+import { SearchBar } from '../../components/SearchBar';
+import { LoginDataItem } from '../../components/LoginDataItem';
+
+import {
+  Container,
+  Metadata,
+  Title,
+  TotalPassCount,
+  LoginList,
+} from './styles';
+
+interface LoginDataProps {
+  id: string;
+  service_name: string;
+  email: string;
+  password: string;
+}
+
+type LoginListDataProps = LoginDataProps[];
+
+export function Home() {
+  const [searchText, setSearchText] = useState('');
+  const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
+  const [data, setData] = useState<LoginListDataProps>([]);
+
+  async function loadData() {
+    const dataKey = '@savepass:logins';
+
+    const stringData = await AsyncStorage.getItem(dataKey);
+
+    if (!stringData) {
+      return;
+    }
+
+    const credentialsList = JSON.parse(stringData);
+
+    setData(credentialsList);
+    setSearchListData(credentialsList);
+  }
+
+  function handleFilterLoginData() {
+    handleChangeInputText(searchText);
+  }
+
+  function handleChangeInputText(text: string) {
+    setSearchText(text);
+
+    if (text.trim() === '' || !searchText) {
+      return setSearchListData(data);
+    }
+
+    const term = searchText.toLowerCase().trim();
+
+    const filteredList = data.filter(({ service_name }: LoginDataProps) =>
+      service_name.toLowerCase().includes(term)
+    );
+
+    setSearchListData(filteredList);
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [])
+  );
+
+  return (
+    <>
+      <Header
+        user={{
+          name: 'Rocketseat',
+          avatar_url: 'https://github.com/faelst.jpg',
+        }}
+      />
+      <Container>
+        <SearchBar
+          placeholder="Qual senha você procura?"
+          onChangeText={handleChangeInputText}
+          value={searchText}
+          returnKeyType="search"
+          onSubmitEditing={handleFilterLoginData}
+          onSearchButtonPress={handleFilterLoginData}
+        />
+
+        <Metadata>
+          <Title>Suas senhas</Title>
+          <TotalPassCount>
+            {searchListData.length
+              ? `${`${searchListData.length}`.padStart(2, '0')} ao total`
+              : 'Nada a ser exibido'}
+          </TotalPassCount>
+        </Metadata>
+
+        <LoginList
+          keyExtractor={(item) => item.id}
+          data={searchListData}
+          renderItem={({ item: loginData }) => {
+            return (
+              <LoginDataItem
+                service_name={loginData.service_name}
+                email={loginData.email}
+                password={loginData.password}
+              />
+            );
+          }}
+        />
+      </Container>
+    </>
+  );
+}
